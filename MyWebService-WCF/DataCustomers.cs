aa8318414b8f0d2118ecee0c32b7c790604573f3
JJ <jj.orozcolopez.13@gmail.com>
2019-07-14 12:44:00 -0600

    Example finalized
    Added connection string.
    Added method in the contract (interface).
    Added Class for Getting Customers' Data from Database.
    Addem method in MyService to call the data to show.

---
+++ MyWebService-WCF/DataCustomers.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyWebService_WCF
+{
+    public class DataCustomers
+    {
+        public DataSet GetCustomers()
+        {
+            DataSet dataTable = new DataSet();
+            
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["NorthwindConnection"].ConnectionString))
+            {
+                con.Open();
+
+                using (SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT [CustomerID],[CompanyName],[ContactName],[ContactTitle],[City],[Country] FROM [Northwind].[dbo].[Customers]", con))
+                {
+                    sqlAdapter.Fill(dataTable);
+                }
+            }
+
+            return dataTable;
+        }
+    }
+}
